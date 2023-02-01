c26087f279c63aad0a8dc04971602669038a5db8
Jozsef Vass <jozsef@discordapp.com>
2023-01-31 18:11:05 -0800

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+Opus Ogg Packing
+================
+
+Reads Opus packets (with RTP header) from a file and creates Ogg container.
+
+Build
+-----
+
+    clang++ -std=c++11 converter.cpp ogg_packer.c
+
---
+++ converter.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "ogg_packer.h"
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cout << "Usage: " << argv[0] << " input_file output_file" << std::endl;
+  }
+}
